afb0368b9a3838fcbed2f79c4c7513381490f240
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-28 19:30:31 -0300

    Adicionada validação se a pessoa é maior ou menor de idade com base na quantidade de anos informada

--- src/desafioProjetoGithub/Main.java
+++ src/desafioProjetoGithub/Main.java
@@ -12,7 +12,16 @@ public class Main {
 		System.out.println("Informe quantos anos você tem:");
 		int anos = scanner.nextInt();
 
-		System.out.println("Meu nome é " + nome + " e tenho " + anos + " anos de idade.");
+		
+		
+		System.out.println("Meu nome é " + nome + " e tenho " + anos + " anos de idade " + maiorOuMenorDeIdade(anos) + ".");
 		scanner.close();
+	}
+	
+	public static String maiorOuMenorDeIdade(int anos) {
+		if(anos >= 18) {
+			return "e sou maior de idade";
+		}
+		return "e não sou maior de idade";
 	}
 }
