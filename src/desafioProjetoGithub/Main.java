ae61c6f7394d58ff7facffcbdc18dee738445561
Gabriel Simon <gabrielsimon775@gmail.com>
2023-01-28 19:23:17 -0300

    Foi Adiciono os scanners para pegar o nome e idade da pessoa

--- src/desafioProjetoGithub/Main.java
+++ src/desafioProjetoGithub/Main.java
@@ -1,5 +1,18 @@
 package desafioProjetoGithub;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Informe seu nome:");
+		String nome = scanner.next();
+
+		System.out.println("Informe quantos anos você tem:");
+		int anos = scanner.nextInt();
+
+		System.out.println("Meu nome é " + nome + " e tenho " + anos + " anos de idade.");
+		scanner.close();
+	}
 }
